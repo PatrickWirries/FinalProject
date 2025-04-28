fe572000d5adf78e7a698c11bafa24b1499100a4
dlginsburg515 <davis.ginsburg@ndsu.edu>
2025-04-27 20:20:59 -0500

    Added Firebase and Service for the inventory database

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
+import { initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwnSna2KL2b6VUDtrls4weRM6nn6rp1UQ",
+  authDomain: "csci-313-final---inventory.firebaseapp.com",
+  projectId: "csci-313-final---inventory",
+  storageBucket: "csci-313-final---inventory.firebasestorage.app",
+  messagingSenderId: "95794925273",
+  appId: "1:95794925273:web:4e51d8d580e00c9183f8c8",
+  measurementId: "G-SMEJSMTK76"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore())
+  ]
 };
