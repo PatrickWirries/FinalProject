732ea3f6462ef8a11a1e0cc2e68468e900cf4981
PatrickWirries <patrickwirries@outlook.com>
2025-05-09 12:01:34 -0500

    Cleaned up a few things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { ForgotPassComponent } from "./forgot-pass/forgot-pass.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, ForgotPassComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
